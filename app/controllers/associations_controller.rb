4ead271b631fc3b1d18bebb0502ffef92c16e1bb
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2019-01-23 10:04:20 +0000

    adds all required stuff for associations

---
+++ app/controllers/associations_controller.rb
@@ -0,0 +1,25 @@
+class AssociationsController < ApplicationController
+
+  def new
+    @association = Association.new
+  end
+
+  def create
+    @association = current_user.associations.build(:associate_id => params[:association][:associate_id])
+    @opp_association = opposite_association(params[:association][:associate_id], current_user.id)
+    if @association.save && @opp_association.save
+      flash[:notice] = "Added associate."
+      redirect_to root_url
+    else
+      flash[:notice] = "Unable to add associate."
+      redirect_to root_url
+    end
+  end
+
+  private
+
+  def opposite_association(associate_id, current_user_id)
+    User.find(associate_id).associations.build(associate_id: current_user_id)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   validates :firstname, presence: true
   validates :lastname, presence: true
 
+  has_many :associations
+  has_many :associates, :through => :associations
+
+  has_many :inverse_associations, :class_name => "association", :foreign_key => "associate_id"
+  has_many :inverse_associations, :through => :inverse_associations, :source => :user
+
   has_secure_password
 end
