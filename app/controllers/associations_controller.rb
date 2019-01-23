c41bdece62b2060afadc5d87c601bdc80b995eb6
Obsiye <mrabdirizak@gmail.com>
2019-01-23 13:45:59 +0000

    Decode qr code and add association

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -9,8 +9,8 @@ class AssociationsController < ApplicationController
   end
 
   def create
-    @association = current_user.associations.build(:associate_id => params[:association][:associate_id])
-    @opp_association = opposite_association(params[:association][:associate_id], current_user.id)
+    @association = current_user.associations.build(:associate_id => params[:associate_id])
+    @opp_association = opposite_association(params[:associate_id], current_user.id)
     if @association.save && @opp_association.save
       flash[:notice] = "Added associate."
       redirect_to associations_path
