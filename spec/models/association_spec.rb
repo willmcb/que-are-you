5ab57c904b7b7024ed34df2da69714df5d199ff8
David Gisbey <davidgisbey@Davids-MBP.lan>
2019-01-25 21:40:02 +0000

    test added for to check for duplicate associates

--- spec/models/association_spec.rb
+++ spec/models/association_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Association, type: :model do
+  before :each do
+  @test_user1 = create(:user)
+  @test_user2 = create(:user, email: 'testuser2@test.com')
+end
 
+  it "cannot add an associate twice" do
+    Association.create(user_id: @test_user2.id, associate_id: @test_user1.id)
+    expect(@test_user2.associations.length).to eq 1
+    Association.create(user_id: @test_user2.id, associate_id: @test_user1.id)
+    expect(@test_user2.associations.length).to eq 1
+  end
 end
