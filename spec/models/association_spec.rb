1e980323f88fcd8d0993a98d83ed280efd67e704
David Gisbey <davidgisbey@Davids-MBP.lan>
2019-01-25 21:44:34 +0000

    test added to check you can't add yourself as an associate

--- spec/models/association_spec.rb
+++ spec/models/association_spec.rb
@@ -6,10 +6,18 @@ RSpec.describe Association, type: :model do
   @test_user2 = create(:user, email: 'testuser2@test.com')
 end
 
+
+  it "cannot add yourself as an associate" do
+    expect(@test_user2.associations.length).to eq 0
+    Association.create(user_id: @test_user2.id, associate_id: @test_user2.id)
+    expect(@test_user2.associations.length).to eq 0
+  end
+
   it "cannot add an associate twice" do
     Association.create(user_id: @test_user2.id, associate_id: @test_user1.id)
     expect(@test_user2.associations.length).to eq 1
     Association.create(user_id: @test_user2.id, associate_id: @test_user1.id)
     expect(@test_user2.associations.length).to eq 1
   end
+
 end
