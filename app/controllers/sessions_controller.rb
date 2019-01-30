c15a413311f7af639da51fc11ccd0e05efdbd2e6
Obsiye <mrabdirizak@gmail.com>
2019-01-30 14:49:17 +0000

    Remove puts command

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -40,7 +40,7 @@ class SessionsController < ApplicationController
     refresh_token = access_token.credentials.refresh_token
 
     user.update_attributes(:google_refresh_token => refresh_token) if refresh_token.present?
-    puts user
+    
     session[:user_id] = user.id.to_s
     redirect_to root_path, notice: 'Successfully connected to Google!'
   end
